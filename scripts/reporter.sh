1c48602e4925f6a4be4128eb6479954a803d3db5
Jason Charcalla <jason@charcalla.com>
2018-04-07 19:00:39 -0500

    almost ready for deployment, linked into ansible

--- scripts/reporter.sh
+++ scripts/reporter.sh
@@ -19,7 +19,7 @@
 CONFIG=/etc/reporter.conf
 ETCD_ENDPOINTS="192.168.79.129:2379,192.168.79.177:2379,192.168.79.178:2379"
 export ETCDCTL_API=3
-ETCDCTL_BIN=/tmp/etcd-v3.2.18-linux-amd64/etcdctl
+ETCDCTL_BIN=/usr/local/bin/etcdctl
 # putting these here for now, they should be in the config file
 REGION=region1
 RACK=rack1
@@ -56,12 +56,15 @@ fi
 # Values should be HASH, IP, FQDN, region, rack
 # /nodes/<region>/<rack>/${HOSTID}
 #echo "${ETCDCTL_BIN} --endpoints=${ETCD_ENDPOINTS} put /nodes/${REGION}/${RACK}/${HOSTID} \"fqdn:${FQDN},ipv4:${IPV4},ipv6:na\""
-${ETCDCTL_BIN} --endpoints=${ETCD_ENDPOINTS} put /nodes/${REGION}/${RACK}/${HOSTID} \"fqdn:${FQDN},ipv4:${IPV4},ipv6:na\"
+# This should only update periodically to allow the scheuler to know the host is live. Needs a counter.
+${ETCDCTL_BIN} --endpoints=${ETCD_ENDPOINTS} put /nodes/${REGION}/${RACK}/${HOSTID} \"fqdn:${FQDN},ipv4:${IPV4},ipv6:na,opts:na\"
 
 # Function for reporting updates on current service status. this was planed to have a TTL, looks like that
 # is done as a lease now. No need to mess with that for the proof of concept.
 report_service()
 {
+	# In reality this function should write to a counter somewhere so the put only occurs every Nth time.
+	# A lease should then be placed on the ket in etcd. this should limit traffic to every 5 to 10 minutes.
 	${ETCDCTL_BIN} --endpoints=${ETCD_ENDPOINTS} put /service/status/${HOSTID}/${line} \"status:active,pid:na\"
 }
 
@@ -90,7 +93,7 @@ restart_service()
 # easily identifiable prefix so we can use a for loop around "systemctl is-active"
 # /service/status/${HOSTID}/<service name>
 #/tmp/etcd-v3.2.18-linux-amd64/etcdctl --endpoints=192.168.79.129:2379,192.168.79.177:2379,192.168.79.178:2379 get --prefix /services/running/f85c215ac6ee4e7d749df30adb986a2804977b3c057f553d29ff959a124efcab | grep enabled| while read -r line; do if [ "$(systemctl is-active $(echo ${line} | cut -d "," -f 1 | cut -d ":" -f2))" = 'active' ]; then "submit function here" ;fi; done
-
+#
 ${ETCDCTL_BIN} --endpoints=${ETCD_ENDPOINTS} get --prefix /services/running/${HOSTID} | grep enabled| while read -r line; do if [ "$(systemctl is-active $(echo ${line} | cut -d "," -f 1 | cut -d ":" -f2))" = 'active' ]; then report_service; else restart_service ;fi; done
 
 exit 0
